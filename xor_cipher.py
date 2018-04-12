68fbc1db99f99a0498604f91b623bed2555ac0d4
12bogdan03 <12bogdan03@gmail.com>
2018-04-12 15:13:25 +0300

    change converting to hex

--- xor_cipher.py
+++ xor_cipher.py
@@ -4,34 +4,38 @@ from itertools import cycle
 
 def xor_convert(text, key):
     try:
-        return ''.join(chr(ord(char) ^ ord(k))
-                       for char, k in zip(text, cycle(key)))
+        return ''.join(hex(ord(char) ^ ord(k)) for char, k in zip(text, cycle(key)))
     except TypeError:
-        return ''.join(chr(ord(char) ^ key) for char in text)
+        return ''.join(hex(ord(char) ^ key) for char in text)
 
 
 if __name__ == '__main__':
-    # key = '99999'
-    # message = 'hello world'
-    # cyphered = xor_convert(message, key)
-    # print('%s ^ %s = %s' % (message, key, cyphered))
-    # decrypted = xor_convert(cyphered, key)
-    # print('%s ^ %s = %s' % (cyphered, key, message))
+    key = 'sfsgjj'
+    message = 'hello world'
+    cyphered = xor_convert(message, key)
+    with open('file1.txt', 'w') as file:
+        file.write(cyphered)
+    print('%s ^ %s = %s' % (message, key, cyphered))
+
+    with open('file1.txt', 'r') as file:
+        content = file.read()
+    decrypted = xor_convert(content, key)
+    print('%s ^ %s = %s' % (content, key, message))
 
     # to run from command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-k', '--key', required=True,
-                        help='A key to perform encryption or decryption.')
-    parser.add_argument('-t', '--text', type=str, required=True,
-                        help='A text to encrypt or decrypt.')
-    parser.add_argument('--key_to_int', action='store_true',
-                        help='Use key as an integer instead of as a string.')
-    args = parser.parse_args()
-
-    if args.key_to_int:
-        args.key = int(args.key)
-
-    print('Converted "{}" to {} using '
-          'key {} .'.format(args.text,
-                            xor_convert(args.text, args.key),
-                            args.key))
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-k', '--key', required=True,
+    #                     help='A key to perform encryption or decryption.')
+    # parser.add_argument('-t', '--text', type=str, required=True,
+    #                     help='A text to encrypt or decrypt.')
+    # parser.add_argument('--key_to_int', action='store_true',
+    #                     help='Use key as an integer instead of as a string.')
+    # args = parser.parse_args()
+    #
+    # if args.key_to_int:
+    #     args.key = int(args.key)
+    #
+    # print('Converted "{}" to {} using '
+    #       'key {} .'.format(args.text,
+    #                         xor_convert(args.text, args.key),
+    #                         args.key))
